05d1077db37234be7dbfe04cf25be94ca8a2fe75
aldairandre <aldairandre99@gmail.com>
2022-12-06 22:10:01 +0000

    ➕ Variables
    Add fundamentals basic

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,9 @@
 import '../lib/say.dart' as say;
-
+import '../lib/var.dart' as math;
 void main(List<String> args) {
   print(say.helloWord());
+  print(math.sum());
+  print(math.sub());
+  print(math.mult());
+  print(math.div());
 }
---
+++ lib/var.dart
@@ -0,0 +1,17 @@
+// Var and basic t
+
+String sum() {
+  return '${10} + ${10} = ${10 + 10}';
+}
+
+String sub() {
+  return '{10} - ${10} = ${10 - 10}';
+}
+
+String mult() {
+  return '{10} * ${10} = ${10 * 10}';
+}
+
+String div() {
+  return '{10} / ${10} = ${10 / 10}';
+}
