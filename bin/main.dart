0373afb5477ea9c95bf9ef9e3128199698362ceb
aldairandre <aldairandre99@gmail.com>
2022-12-07 19:46:43 +0000

    ➕ The basic primitive types

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,25 @@
 import '../lib/say.dart' as say;
 import '../lib/var.dart' as math;
+
 void main(List<String> args) {
+  //Primitive Types
+  const String name = 'Aldair';
+  num anyNumber = 19.3; //also can change to non-floating number
+  const int age = 22;
+  const double height = 1.77;
+
+  //Print method
   print(say.helloWord());
+
+  // Simple calculator
   print(math.sum());
   print(math.sub());
   print(math.mult());
   print(math.div());
+
+  //Using the basic primitive types
+  print('Name: $name');
+  print('Any Number: $anyNumber');
+  print('Age: $age');
+  print('Height: $height');
 }
