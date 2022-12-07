80e6b19a81060e9ea4ae92e700d5bc4fadd4e055
aldairandre <aldairandre99@gmail.com>
2022-12-07 09:49:58 +0000

    ♻️ Refactor Variables

--- lib/var.dart
+++ lib/var.dart
@@ -1,21 +1,21 @@
 // Introductions Variables
 
 String sum() {
-  var n1 = 10;
+  int n1 = 9;
   return '$n1 + $n1 = ${n1 + n1}';
 }
 
 String sub() {
-  var n1 = 10;
+  int n1 = 10;
   return '$n1 - $n1 = ${n1 - n1}';
 }
 
 String mult() {
-  var n1 = 10;
+  int n1 = 10;
   return '${n1} * ${n1} = ${n1 * n1}';
 }
 
 String div() {
-  var n1 = 10;
+  int n1 = 10;
   return '${n1} / ${n1} = ${n1 / n1}';
 }
